e600248e71ca9bb65c56543156c26772987a798e
Alexandr <justdealman@gmail.com>
2014-07-23 01:49:09 +0700

    new header and slider button design

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,45 +1,23 @@
 ﻿$(document).ready(function() {
-	var headertop = $('.header').offset().top;
+	var headertop = 134;
 	$(window).bind('scroll', function() {
 		if ( $(document).scrollTop() > headertop ) {
-			$('.header').css({
-				'position': 'fixed',
-				'height': '68px'
-			});
-			$('.header > div > ul li a').css({
-				'padding': '20px 43px 20px'
-			});
-			$('.header > div > ul li ul').css({
-				'top': '68px'
-			});
-			$('.panel').css({
-				'margin-bottom': '134px'
-			});
+			$('.panel').addClass('fixed');
+			$('.wrapper').css({'padding-top': '180px'});
 		}
 		else {
-			$('.header').css({
-				'position': 'relative',
-				'height': '88px'
-			});
-			$('.header > div > ul li a').css({
-				'padding': '30px 43px 30px'
-			});
-			$('.header > div > ul li ul ').css({
-				'top': '88px'
-			});
-			$('.panel').css({
-				'margin-bottom': '0'
-			});
+			$('.panel').removeClass('fixed');
+			$('.wrapper').css({'padding-top': '0'});
 		}
 	});
-	$('.header > div > ul li').hover(
+	$('.panel .nav > li:has(ul)').hover(
 		function() {
 			$(this).addClass('active');
-			$(this).children('ul').stop(true,true).slideDown(150);
+			$(this).children('ul').stop(true,true).slideDown(0);
 		},
 		function() {
 			$(this).removeClass('active');
-			$(this).children('ul').stop(true,true).slideUp(150);
+			$(this).children('ul').stop(true,true).slideUp(0);
 		}
 	);
 });
